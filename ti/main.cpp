fda1e0ef7f815e53f3217f0a71912e92830ff176
20181102932 <908907990@qq.com>
2019-06-13 10:52:26 +0800

    Initial Commit

---
+++ ti/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ti
+//
+//  Created by s20181102932 on 2019/6/13.
+//  Copyright © 2019 s20181102932. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
